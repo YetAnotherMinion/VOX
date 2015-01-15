34bc2312b4c8400e7beae2f414a8edf6a6bf671b
YetAnotherMinion <yetanotherminion@gmail.com>
2015-01-15 12:13:37 -0500

    minor changes

---
+++ server/inc/ellipsoid.h
@@ -0,0 +1,12 @@
+#ifndef _ELLIPSOID_CAD_
+#define _ELLIPSOID_CAD_
+
+#include <stdint.h>
+//To voxelise a surface we test for voxels that have a
+//center inside the surface.
+
+
+
+void ellipseLinearGenerator(uint32_t A, uint32_t B)
+
+#endif

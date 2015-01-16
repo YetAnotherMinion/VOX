9180d2dcf25f7d84315ba7acb60b392cbd31eace
YetAnotherMinion <yetanotherminion@gmail.com>
2015-01-16 17:52:25 -0500

    switched to pedantic

--- server/inc/ellipsoid.h
+++ server/inc/ellipsoid.h
@@ -2,11 +2,23 @@
 #define _ELLIPSOID_CAD_
 
 #include <stdint.h>
-//To voxelise a surface we test for voxels that have a
-//center inside the surface.
+/*To voxelise a surface we test for voxels that have a
+center inside the surface. */
 
 
+struct Point2D {
+	uint32_t x;
+	uint32_t y;
+};
 
-void ellipseLinearGenerator(uint32_t A, uint32_t B);
+struct Point3D {
+	uint32_t x;
+	uint32_t y;
+	uint32_t z;
+};
+
+void ellipseLinearGenerator(uint32_t A, uint32_t B, struct Point2D* result);
+
+
 
 #endif
--- server/src/server.c
+++ server/src/server.c
@@ -4,10 +4,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <errno.h>
-//gives you htons()
+/*gives you htons() */
 #include <arpa/inet.h>
 
-//locally defined files
+/*locally defined files */
 #include "chunks.h"
 #include "ellipsoid.h"
 
@@ -16,13 +16,17 @@ int main(int argc, char *argv[])
 	if(argc != 3) {
 		printf("Usage: %s <address> <port>\n",argv[0]);
 	}
-	int socketfd, newsockfd, portno, client_length;
-	struct sockaddr_in server_addr, client_addr;
+	int socketfd;
+	int newsockfd;
+	int portno;
+	int client_length;
+	struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
 	int n;
 	char buffer[256];
-	//open the socket
+	/*open the socket */
 	socketfd = socket(AF_INET, SOCK_STREAM, 0);
-	//ckeck that the socket is valid
+	/*ckeck that the socket is valid */
 	if(socketfd < 0) {
 		printf("Error in socket creation\n");
 		exit(1);
@@ -30,17 +34,17 @@ int main(int argc, char *argv[])
 
 	portno = atoi(argv[2]);
 	server_addr.sin_family = AF_INET;
-	//add the ip address of the socket
+	/*add the ip address of the socket */
 	server_addr.sin_addr.s_addr = inet_addr(argv[1]);
-	//convert the bost byte order and network byte order with htons()
+	/*convert the bost byte order and network byte order with htons() */
 	server_addr.sin_port = htons(portno);
-	//we will need to recast the sockaddr_in struct to general
-	//type sockaddr
+	/*we will need to recast the sockaddr_in struct to general */
+	/*type sockaddr */
 	if(bind(socketfd, (const struct sockaddr *) &server_addr, sizeof(server_addr)) < 0 ) {
 		printf("error in binding\n");
 		exit(1);
 	}
-	//start listening for clients
+	/*start listening for clients */
 
 	listen(socketfd,5);
 

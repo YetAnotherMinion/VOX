16245ae7af8455e0bb33f0831d94034122745e20
Another Minion <yetanotherminion@gmail.com>
2015-01-17 23:24:04 -0500

    added prototypes
    fleshing out how the ellipse voxelization will work

--- server/inc/ellipsoid.h
+++ server/inc/ellipsoid.h
@@ -16,9 +16,22 @@ struct Point3D {
 	uint32_t y;
 	uint32_t z;
 };
+/*For the linear case where we are moving along an ellipse in plane*/
+struct Ellipse2DClosure {
+	uint32_t A;
+	uint32_t B;
+	uint64_t x;
+	uint64_t y;
+};
+/*For when we are moving along a surface*/
+
 
 void ellipseLinearGenerator(uint32_t A, uint32_t B, struct Point2D* result);
-
-
+/*get only the next point from the point provided in the closure */
+void ellipse2DNext(struct Ellipse2DClosure* closure, struct Point2D* result);
+/*get only the next point on the surface in the direction defined
+direction = 0 -> move vector is along pos y
+direction = 1 -> move vector is along pos z */
+void ellipse3DNext(struct Ellipse3DClosure* closure, struct Point3D result, bool direction)
 
 #endif

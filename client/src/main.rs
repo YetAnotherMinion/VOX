7d4ed26732396413ee2cf7ac90e4976b8248fcce
YetAnotherMinion <yam@thinkalexandria.com>
2016-08-27 14:58:18 -0400

    animate the triangle moving across the screen

--- client/src/main.rs
+++ client/src/main.rs
@@ -18,40 +18,54 @@ implement_vertex!(ArbitraryNameVertex, position);
 
 fn main() {
     let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+        
+    let vertex1 = ArbitraryNameVertex { position: [-0.5, -0.5] };
+    let vertex2 = ArbitraryNameVertex { position: [0.0, 0.5] };
+    let vertex3 = ArbitraryNameVertex { position: [0.5, -0.25] };
+
+    let shape = vec![ vertex1, vertex2, vertex3];
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+
+    let mut t: f32 = -0.5;
+
+    let vertex_shader_src = r#"
+        #version 130
+        in vec2 position;
+        uniform float t;
+        void main() {
+            vec2 pos =  position;
+            pos.x += t;
+            gl_Position = vec4(pos, 0.0, 1.0);
+        }
+    "#;
+    
+    let fragment_shader_src = r#"
+        #version 130
+        out vec4 color;
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
     // prevent the window from immediately closing by polling forever until we
     // recieve a `Closed` event
     loop {
+        t += 0.02;
+        if t > 0.5 {
+            t = -0.5;
+        }
         // draw a test image
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
 
-        let vertex1 = ArbitraryNameVertex { position: [-0.5, -0.5] };
-        let vertex2 = ArbitraryNameVertex { position: [0.0, 0.5] };
-        let vertex3 = ArbitraryNameVertex { position: [0.5, 0.25] };
-
-        let shape = vec![ vertex1, vertex2, vertex3];
-        let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
         let indicies = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-        let vertex_shader_src = r#"
-            #version 130
-            in vec2 position;
-            void main() {
-                gl_Position = vec4(position, 0.0, 1.0);
-            }
-        "#;
         
-        let fragment_shader_src = r#"
-            #version 130
-            out vec4 color;
-            void main() {
-                color = vec4(1.0, 0.0, 0.0, 1.0);
-            }
-        "#;
-        
-        let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+        let program = glium::Program::from_source(&display,
+                                                  vertex_shader_src,
+                                                  fragment_shader_src,
+                                                  None).unwrap();
 
         target.draw(&vertex_buffer, &indicies, &program,
-                    &glium::uniforms::EmptyUniforms,
+                    &uniform! { t: t },
                     &Default::default()).unwrap();
         target.finish().unwrap();
 

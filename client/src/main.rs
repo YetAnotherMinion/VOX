29c3279cf3068fcb3d3cf5248255c4fb421db68d
YetAnotherMinion <yam@thinkalexandria.com>
2016-08-27 11:12:31 -0400

    window opening demo works

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+#[macro_use]
+extern crate glium;
+
+// We want to create a window with an OpenGL context handled by glium, instead
+// of calling `build()` we will call `build_glium()` which is defined  in the
+// `glium::DisplayBuild` trait
+use glium::DisplayBuild;
+
+fn main() {
+    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+    // preven the window from immediately closing by polling forever until we
+    // recieve a `Closed` event
+    loop {
+        for ev in display.poll_events() {
+            match ev {
+                glium::glutin::Event::Closed => return, // window closed by user
+                _ => (),
+            }
+        }
     }
 }
